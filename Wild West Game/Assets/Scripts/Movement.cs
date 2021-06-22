b18308b589dda7d1356a4c76f7862accf980887b
Taren P <63258880+TarenP@users.noreply.github.com>
2021-06-22 10:18:40 -0700

    ground and movement added

---
+++ Wild West Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public int speed = 5;
+    Rigidbody rb;
+    float jumpForce = 4.0f;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+                        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.A))
+                        transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+                if(Input.GetKey(KeyCode.D))
+                        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+                if(Input.GetKey(KeyCode.S))
+                        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+                if(Input.GetKey(KeyCode.Space) && jump==false){
+                    rb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+                    jump = true;
+                }
+    }
+
+    void OnCollisionEnter(Collision other){
+            if(other.gameObject.CompareTag("Floor")){
+                jump = false;
+            }
+        }
+}
