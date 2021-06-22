1b12ad980630894254ef23242063360f95792d5a
Taren P <63258880+TarenP@users.noreply.github.com>
2021-06-22 11:19:27 -0700

    Added Player cowboy mesh with morphed hand to hold gun

--- Wild West Game/Assets/Scripts/Movement.cs
+++ Wild West Game/Assets/Scripts/Movement.cs
@@ -20,16 +20,16 @@ public class Movement : MonoBehaviour
     void Update()
     {
         if(Input.GetKey(KeyCode.W))
-                        transform.Translate(Vector3.left * speed * Time.deltaTime);
-
-        if(Input.GetKey(KeyCode.A))
-                        transform.Translate(Vector3.back * speed * Time.deltaTime);
-
-                if(Input.GetKey(KeyCode.D))
                         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
-                if(Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.A))
+                        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+                if(Input.GetKey(KeyCode.D))
                         transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+                if(Input.GetKey(KeyCode.S))
+                        transform.Translate(Vector3.back * speed * Time.deltaTime);
 
                 if(Input.GetKey(KeyCode.Space) && jump==false){
                     rb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
