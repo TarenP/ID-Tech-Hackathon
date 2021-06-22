c422afa7565933d3ee340ad9d558f374457a3adc
Taren P <63258880+TarenP@users.noreply.github.com>
2021-06-22 12:51:31 -0700

    Mouse Look Update

--- Wild West Game/Assets/Scripts/Movement.cs
+++ Wild West Game/Assets/Scripts/Movement.cs
@@ -7,34 +7,62 @@ public class Movement : MonoBehaviour
 
     public int speed = 5;
     Rigidbody rb;
-    float jumpForce = 4.0f;
+    public float jumpForce = 4.0f;
     bool jump = false;
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 3.5f;
+
+    [SerializeField] bool lockCursor = true;
+
+    float cameraPitch = 0.0f;
+
+    public float minClampy = -90f;
+    public float maxClampy = 90f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        if(lockCursor = true)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        MouseLook();
         if(Input.GetKey(KeyCode.W))
                         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         if(Input.GetKey(KeyCode.A))
                         transform.Translate(Vector3.left * speed * Time.deltaTime);
 
-                if(Input.GetKey(KeyCode.D))
-                        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.D))
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
 
-                if(Input.GetKey(KeyCode.S))
-                        transform.Translate(Vector3.back * speed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.S))
+                transform.Translate(Vector3.back * speed * Time.deltaTime);
 
-                if(Input.GetKey(KeyCode.Space) && jump==false){
-                    rb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
-                    jump = true;
-                }
+        if(Input.GetKey(KeyCode.Space) && jump==false){
+            rb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+            jump = true;
+        }
+    }
+
+    void MouseLook()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        cameraPitch -= mouseDelta.y * mouseSensitivity;
+
+        cameraPitch = Mathf.Clamp(cameraPitch, minClampy, maxClampy);
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
     }
 
     void OnCollisionEnter(Collision other){
